191d7cac0731a334075830254cd1cdf6e3366ba7
Sivapriya K <40844458+sivapriya-kumaresan@users.noreply.github.com>
2023-08-15 19:40:54 +0530

    Update Testsuite.py

--- Testsuite.py
+++ Testsuite.py
@@ -6,9 +6,9 @@ def run_script(self,testcase_name):
     subprocess.run(["python", testcase_name])
 
 for i in range(1,3):
-    print(f"Running Testcase{script}.py")
+    print(f"Running Testcase{i}.py")
     testcase = f"Testcase{i}.py"
     run_script(testcase)
-    print(f"Testcase{script} execution complete")
+    print(f"Testcase{i} execution complete")
     print("Wait for 10 seconds before executing next testcase")
     time.sleep(10)
