5eda932dde4fb7973226b6450153dbf6f5e12a8f
Sivapriya K <40844458+sivapriya-kumaresan@users.noreply.github.com>
2023-08-29 22:02:33 +0530

    Create Testcase5.py

---
+++ Testcase5.py
@@ -0,0 +1,9 @@
+import xml.etree.ElementTree as ET
+
+xml_string = "<root><element1>Value 1</element1><element2>Value 2</element2>"
+
+try:
+    # Parse XML string
+    root = ET.fromstring(xml_string)
+except ET.ParseError as e:
+    print("XMLParseError:", e)
