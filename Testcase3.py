124eb44e841a439935cdfbabe351ea2e1c04cbd4
Sivapriya K <40844458+sivapriya-kumaresan@users.noreply.github.com>
2023-08-29 21:08:22 +0530

    Create Testcase3.py

---
+++ Testcase3.py
@@ -0,0 +1,19 @@
+import paramiko
+
+ssh_username = "127.0.0.1"
+ssh_password = "admin"
+ssh_server = "localhost"
+
+ssh_client = paramiko.SSHClient()
+
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh_client.connect(hostname=ssh_server, username=ssh_username, password=ssh_password)
+
+command = "ls -l"
+stdin, stdout, stderr = ssh_client.exec_command(command)
+
+print("Command Output:")
+print(stdout.read().decode("utf-8"))
+
+ssh_client.close()
