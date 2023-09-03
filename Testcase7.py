3ac342368edc359cb9dc6f14e0b22d8d18d58f9b
Sivapriya K <40844458+sivapriya-kumaresan@users.noreply.github.com>
2023-09-03 21:46:39 +0530

    Create Testcase7.py

---
+++ Testcase7.py
@@ -0,0 +1,35 @@
+import os
+try:
+    import requests
+except:
+    os.system("python -m pip install requests")
+    import requests
+
+
+HOST = 'ios-xe-mgmt-latest.cisco.com'
+PORT = 443
+USER = 'admin'
+PASS = 'C1sco12345'
+
+# disable urlib3 warning
+requests.packages.urllib3.disable_warnings()
+
+headers = {'Content-Type': 'application/yang-data+json',
+'Accept': 'application/yang-data+json'}
+
+url = f"https://{HOST}/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces/interface=GigabitEthernet1/admin-status"
+response = requests.get(url, auth=(USER, PASS), headers=headers, verify=False)
+print(response.text)
+if "if-state-down" in response.text:
+  print("Verification Success")
+else:
+  print("Expected admin-status if-state-down not found in response")
+  
+url = f"https://{HOST}/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces/interface=GigabitEthernet1/oper-status"
+response = requests.get(url, auth=(USER, PASS), headers=headers, verify=False)
+print(response.text)
+if "if-oper-state-ready" in response.text:
+  print("Verification Success")
+else:
+  print("Expected oper-status if-oper-state-ready not found in response")
+  
