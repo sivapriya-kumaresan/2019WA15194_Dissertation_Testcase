b27b4a7ffa84b621fce3583a36705a79f3c96cf7
Sivapriya K <40844458+sivapriya-kumaresan@users.noreply.github.com>
2023-10-08 18:17:37 +0530

    Update Testcase4.py

--- Testcase4.py
+++ Testcase4.py
@@ -1,5 +1,2 @@
-try:
-    with open('non_existent_file.txt', 'r') as file:
-        content = file.read()
-except FileNotFoundError as e:
-    print("FileNotFoundError:", e)
+with open('non_existent_file.txt', 'r') as file:
+    content = file.read()
