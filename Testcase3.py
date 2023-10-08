f8e341a46cb35b2879d56c496d32e9f18126f26b
Sivapriya K <40844458+sivapriya-kumaresan@users.noreply.github.com>
2023-10-08 18:38:38 +0530

    Update Testcase3.py

--- Testcase3.py
+++ Testcase3.py
@@ -10,14 +10,17 @@ ssh_username = "127.0.0.1"
 ssh_password = "admin"
 ssh_server = "localhost"
 
-ssh_client = paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-print(f"Establish connection to {ssh_username}")
-ssh_client.connect(hostname=ssh_server, username=ssh_username, password=ssh_password)
-command = "ls -l"
-stdin, stdout, stderr = ssh_client.exec_command(command)
-    
-print("Command Output:")
-print(stdout.read().decode("utf-8"))
-ssh_client.close()
+try:
+    ssh_client = paramiko.SSHClient()
 
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    print(f"Establish connection to {ssh_username}")
+    ssh_client.connect(hostname=ssh_server, username=ssh_username, password=ssh_password)
+
+    command = "ls -l"
+    stdin, stdout, stderr = ssh_client.exec_command(command)
+
+    print("Command Output:")
+    print(stdout.read().decode("utf-8"))
+except NoValidConnectionsError as e:
+    print("No valid connections available:", e)
