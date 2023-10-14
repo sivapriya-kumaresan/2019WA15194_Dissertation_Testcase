45e10ef5d6db961b19afe864563397001705a473
Sivapriya K <40844458+sivapriya-kumaresan@users.noreply.github.com>
2023-10-14 20:43:29 +0530

    Update Failure_prediction.py

--- Failure_prediction.py
+++ Failure_prediction.py
@@ -30,6 +30,7 @@ def accuracy(model,test,predicted):
     countCorrect = 0
     
     for i in range(0,count):
+        print("test[i] == ",test[i])
         ytest = model[model['FAILURE'] == test[i]]
         print("TEST == ",ytest)
         print("PREDICTED == ",predicted[i])
