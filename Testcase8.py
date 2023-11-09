24375a8c5ca4e9c1367f664986c4629f9b97c860
Sivapriya K <40844458+sivapriya-kumaresan@users.noreply.github.com>
2023-11-09 10:09:09 +0530

    Create Testcase8.py

---
+++ Testcase8.py
@@ -0,0 +1,38 @@
+import os
+try:
+    import netmiko
+    import paramiko
+except:
+    os.system("python -m pip install netmiko")
+    os.system("python -m pip install paramiko")
+    import netmiko
+    import paramiko
+
+from netmiko import ConnectHandler
+from paramiko import SSHException
+
+# Define Cisco device details with incorrect credentials
+device = {
+    'device_type': 'cisco_ios',
+    'ip': 'ios-xe-mgmt-latest.cisco.com',
+    'username': 'admin',
+    'password': 'Cisco12345',
+}
+
+try:
+    # Attempt to establish SSH connection to the device
+    net_connect = ConnectHandler(**device)
+
+    # If the connection is successful, this won't be reached
+    print("Connected to the device successfully.")
+
+except SSHException as e:
+    print(f"Authentication Error: {str(e)}")
+
+except Exception as e:
+    print(f"An unexpected error occurred: {str(e)}")
+
+finally:
+    # Close the SSH connection if it was established
+    if 'net_connect' in locals():
+        net_connect.disconnect()
